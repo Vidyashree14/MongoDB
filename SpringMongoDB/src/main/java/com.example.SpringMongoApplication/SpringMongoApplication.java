3c4e3a88d56d758129d46e77594df659c30a7344
Vidyashree14 <39667604+Vidyashree14@users.noreply.github.com>
2018-06-16 15:24:51 +0400

    Create SpringMongoApplication.java

---
+++ SpringMongoDB/src/main/java/com.example.SpringMongoApplication/SpringMongoApplication.java
@@ -0,0 +1,16 @@
+/**
+  * Created by Vidyashree on 04/28/2018
+  */
+  
+package com.example.SpringMongoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoApplication.class, args);
+	}
+}
